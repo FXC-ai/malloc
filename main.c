2a8d6a13979c307664a60999c6a3d25eea70e0cf
Francois-xavier Coindreau <fcoindre@student.42.fr>
2024-06-25 15:41:05 +0200

    first push

---
+++ main.c
@@ -0,0 +1,85 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+
+int test_malloc()
+{
+	int count = 0;
+	while (malloc(1000))
+	{
+		++count;
+		if (count % 1000000 == 0)
+			printf("count = %d\n", count);
+			
+	}
+	printf("count final = %d\n", count);
+	return count;
+}
+
+int main()
+{
+	int pageSize = getpagesize();
+	printf("getpagesize = %d\n", pageSize);
+
+	int pid = getpid();
+	printf("pid = %d\n", pid);
+
+	// const char *fileName = "maped_memory.txt";
+
+	// const int size_file = 25;
+
+	char *ptr_test = (char *) malloc (sizeof(char) * 4);
+
+	// ptr_test[0] = 'a';
+	// ptr_test[1] = 'a';
+	// ptr_test[2] = 'a';
+	// ptr_test[3] = '\0';
+
+
+	int c = 0;
+
+	while (c < 3)
+	{
+		ptr_test[c] = 'a';
+		c++;
+	}
+	ptr_test[c] = '\0';
+	printf("ptr_test : %s\n", ptr_test);
+
+
+
+	int fd = open(fileName, O_RDWR | O_CREAT, 0400 | 0200);
+
+	if (fd == -1)
+	{
+		perror("open");
+		return 1;
+	}
+
+	if (ftruncate(fd, size_file) == -1)
+	{
+		perror("ftruncate");
+		return 1;
+	}
+
+	void *file_memory = mmap(NULL, size_file, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (file_memory == MAP_FAILED)
+	{
+		perror("mmap");
+		close(fd);
+		return 1;
+	}
+
+	const char * msg = "only a test";
+	strncpy(file_memory, msg, strlen(msg));
+
+	printf("content of the file memory : %s",(char *)file_memory);
+
+	munmap(file_memory, size_file);
+	close(fd);
+
+	return 0;
+}
